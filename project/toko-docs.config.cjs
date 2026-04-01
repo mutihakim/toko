035b50459fef482a50ae29e435339b4815b5602a
Admin <admin@example.com>
2026-04-01 01:50:47 +0000

    fix: correct docs path in pm2 config

--- project/toko-docs.config.cjs
+++ project/toko-docs.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [{
         name: "toko-docs",
         script: "npx",
-        args: "http-server docs/.vitepress/dist -p 8017",
+        args: "http-server ../docs/.vitepress/dist -p 8017",
         cwd: __dirname
     }]
 };
